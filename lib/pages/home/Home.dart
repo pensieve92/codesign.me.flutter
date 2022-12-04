29f53044fdd0d98e5aaa14952a77a1c3fd89f1ba
byeonghyeon.jeon <pensieve92@gmail.com>
2022-12-04 13:26:01 +0900

    Add setToday

--- lib/pages/home/Home.dart
+++ lib/pages/home/Home.dart
@@ -15,52 +15,54 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          onPressed: () {
-            print('click menu icon');
-          },
-          icon: Icon(Icons.menu_rounded),
-        ),
-        actions: [
+      appBar: CalendarAppBar(context),
+      body: Text('Body'), // TODO Calendar 만들기
+    );
+  }
 
-          TextButton(
-              child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const [
-                    Text('오늘',style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
-                    Icon(Icons.arrow_forward_ios_rounded, color: Colors.grey, )]),
-              onPressed: () {
-                print('click 오늘');
-              },
-          ),
-          IconButton(
-            onPressed: () {
-              print('click search icon');
-            },
-            icon: Icon(Icons.search_rounded),
-          ),
-          IconButton(
-            onPressed: () {
-              print('click calendar month icon');
-            },
-            icon: Icon(Icons.calendar_month_rounded),
-          ),
-        ],
-        title: DropdownButton<String>(
-          style: TextStyle(color: Colors.white),
-          items: context.watch<CalendarStore>().yearMonthList.map<DropdownMenuItem<String>>((String item) {
-            return DropdownMenuItem<String>(
-              value: item,
-              child: Text(item),
-            );
-          }).toList(),
-          value: context.watch<CalendarStore>().selectedYearMonth,
-          onChanged: (value) => context.read<CalendarStore>().setYearMonth(value),
-        ),
-        backgroundColor: Colors.black,
+  AppBar CalendarAppBar(BuildContext context) {
+    return AppBar(
+      leading: IconButton(
+        onPressed: () {
+          print('click menu icon');
+        },
+        icon: Icon(Icons.menu_rounded),
       ),
-      body: Text('Body'),
+      actions: [
+
+        TextButton(
+            child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  Text('오늘',style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+                  Icon(Icons.arrow_forward_ios_rounded, color: Colors.grey, )]),
+            onPressed: () => context.read<CalendarStore>().setToday(),
+        ),
+        IconButton(
+          onPressed: () {
+            print('click search icon');
+          },
+          icon: Icon(Icons.search_rounded),
+        ),
+        IconButton(
+          onPressed: () {
+            print('click calendar month icon');
+          },
+          icon: Icon(Icons.calendar_month_rounded),
+        ),
+      ],
+      title: DropdownButton<String>( // TODO Calendar Popup띄우기
+        style: TextStyle(color: Colors.grey, fontWeight: FontWeight.bold),
+        items: context.watch<CalendarStore>().yearMonthList.map<DropdownMenuItem<String>>((String item) {
+          return DropdownMenuItem<String>(
+            value: item,
+            child: Text(item),
+          );
+        }).toList(),
+        value: context.watch<CalendarStore>().selectedYearMonth,
+        onChanged: (value) => context.read<CalendarStore>().setYearMonth(value),
+      ),
+      backgroundColor: Colors.black,
     );
   }
 }
--- lib/stores/CalendarStore.dart
+++ lib/stores/CalendarStore.dart
@@ -4,10 +4,43 @@ class CalendarStore extends ChangeNotifier {
   var yearMonthList = ['2022.11', '2022.12', '2023.01', '2023.02'];
   var selectedYearMonth;
 
-  CalendarStore(): selectedYearMonth = '2022.12';
+  //현재 연월로 초기화
+  CalendarStore(): selectedYearMonth = [DateTime.now().year, DateTime.now().month].join('.') ;
 
   setYearMonth(String? value) {
     selectedYearMonth = value;
     notifyListeners();
   }
+
+  setToday(){
+    DateTime now = DateTime.now();
+    setDay(now.year, now.month, now.day);
+  }
+
+  setDay(int year, int month, int day){
+    selectedYearMonth = [year, month].join('.');
+    String weekday = getWeekday(year, month, day, null);
+    notifyListeners();
+
+    print('setDay $selectedYearMonth$day : $weekday');
+  }
+
+
+  /**
+   * 요일 조회
+   */
+  getWeekday(int year, int month, int day, String? language){
+    return weekday[DateTime(year, month, day).weekday]![language ?? 'ko'] ?? 'Error';
+  }
+
 }
+
+const weekday = {
+  1: {'en': 'monday', 'ko': '월요일'},
+  2: {'en': 'tuesday', 'ko': '화요일'},
+  3: {'en': 'wednesday', 'ko': '수일'},
+  4: {'en': 'thursday', 'ko': '목요일'},
+  5: {'en': 'friday', 'ko': '금요일'},
+  6: {'en': 'saturday', 'ko': '토요일'},
+  7: {'en': 'sunday', 'ko': '월요일'},
+};
