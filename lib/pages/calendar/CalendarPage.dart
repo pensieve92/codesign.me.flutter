2ce2a2fdc41cafb7594720db1ab3c006bf9b84b1
byeonghyeon.jeon <pensieve92@gmail.com>
2023-01-27 22:10:17 +0900

    Fix async, 소스 정리

--- lib/pages/calendar/CalendarPage.dart
+++ lib/pages/calendar/CalendarPage.dart
@@ -17,27 +17,17 @@ class CalendarPage extends StatefulWidget {
 
 class _CalendarPageState extends State<CalendarPage> {
   var gridCellRatio = 1 / 2; // 가로 제로 비율
-  final ScrollController _scrollController = ScrollController();
   final _docContentEditController = TextEditingController();
 
+  final CalendarStore calendarStore = CalendarStore();
+  late Future<List<Day>>? daysOfMonth;
 
   @override
   void initState() {
     super.initState();
-
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: baseCalendarAppBar(context),
-      body: FutureBuilder(
-        future: context.watch<CalendarStore>().selectedMonthCalendar,
-        builder: (BuildContext context, AsyncSnapshot<List<Day>> snapshot) {
-          return baseCalendarBody(context, snapshot);
-        },),
-      bottomSheet: baseCalendarBottomSheet(context),
-    );
+    setState(() {
+      daysOfMonth = calendarStore.makeCalendar();
+    });
   }
 
   @override
@@ -46,161 +36,26 @@ class _CalendarPageState extends State<CalendarPage> {
     super.dispose();
   }
 
-  /**
-   * 캘린더 - 하단 검색바
-   */
-  Row baseCalendarBottomSheet(BuildContext context) {
-    return Row(
-      children: [
-        Flexible(
-          child: TextField(
-            // TODO
-            // 커서가 올라가면 borderRadius가 적용이 안된다.
-            // 커서올라같을때 시타일 지정하는게 있는지 찾아보기
-            decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.blue.shade100,
-                enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(30),
-                    borderSide: BorderSide(
-                      color: Colors.green,
-                      width: 1.0,
-                    ))),
-            controller: _docContentEditController,
-          ),
-        ),
-        ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              foregroundColor:
-              Theme
-                  .of(context)
-                  .colorScheme
-                  .onSecondaryContainer,
-              backgroundColor: Theme
-                  .of(context)
-                  .colorScheme
-                  .secondaryContainer,
-            ).copyWith(elevation: ButtonStyleButton.allOrNull(0.0)),
-            onPressed: () {},
-            // 키보드 활성 여부에 따라 버튼 아이콘 변경
-            child: gf_isShowKeyboard(context)
-                ? IconButton(
-              icon: Icon(Icons.check),
-              onPressed: () {
-                print('click check butotn ');
-                // saveDoc DocType >>  일정, 메모, , todo, note post
-                context.read<CalendarStore>().saveDoc(
-                    _docContentEditController.text);
-              },
-            )
-                : IconButton(
-              icon: Icon(Icons.add),
-              onPressed: () {
-                // TODO 상세 입력폼 나오게 분리하기
-                print('click add butotn ');
-              },
-            )),
-      ],
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: baseCalendarAppBar(context),
+      body: FutureBuilder(
+        future: daysOfMonth,
+        builder: (BuildContext context, AsyncSnapshot<List<Day>> snapshot) {
+          if (snapshot.hasData) {
+            return baseCalendarBody(context, snapshot);
+          } else {
+            return CustomScrollView();
+          }
+          ;
+        },
+      ),
+      bottomSheet: baseCalendarBottomSheet(context),
     );
   }
 
-  CustomScrollView baseCalendarBody(BuildContext context, AsyncSnapshot<List<Day>> snapshot) {
-    var days = snapshot.data;
-
-    return CustomScrollView(
-      // controller: _scrollController,
-      scrollDirection: Axis.vertical,
-      slivers: <Widget>[
-        // 월 ~ 일 달력 헤더 그리드
-        SliverGrid(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 7, // 1개의 행에 보여줄 item개수
-            childAspectRatio:
-            2.5 / 1, // item의 가로세로비율 가로/세로 // 이거를 스크롤 될때 줄이거나 늘리거나 해야됨!
-          ),
-          delegate: SliverChildBuilderDelegate(
-                (BuildContext context, int index) {
-              return Container(
-                height: 100,
-                alignment: Alignment.center,
-                color: Colors.black,
-                child: Text(getWeekdays()[index].name,
-                    style: TextStyle(color: getWeekdays()[index].color)),
-              );
-            },
-            childCount: 7,
-          ),
-        ),
-        // 달력 메인 그리드
-        SliverGrid(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 7, // 1개의 행에 보여줄 item개수
-            childAspectRatio: gridCellRatio ??
-                1 / 2, // item의 가로세로비율 가로1: 세로2 // 이거를 스크롤 될때 줄이거나 늘리거나 해야됨! //
-          ),
-          delegate: SliverChildBuilderDelegate(
-                (BuildContext context, int index) {
-              // DayWidget
-              return Container(
-                alignment: Alignment.center,
-                color: Colors.black,
-                child: GestureDetector(
-                  onTap: () =>
-                      context.read<CalendarStore>().setSelectedDay(index),
-                  child: Opacity(
-                    opacity: days![index].isCurMonth ? 1.0 : 0.5,
-                    child: Container(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: DayWidget(context, days!, index),
-                      ),
-                      decoration: setSelectedDayStyle(index),
-                    ),
-                  ),
-                ),
-              );
-            },
-            childCount: 42, // TODO 42로 다시 변경해야함
-          ),
-        ),
-
-        // SliverFixedExtentList(delegate: SliverChildBuilderDelegate((BuildContext context, int index){
-        //   return ListTile(title: Text('List Item'));
-        // }), itemExtent: 10.0),
-
-        // bottomSheet 높이만큼 height를 지정해주자
-        SliverToBoxAdapter(
-          child: Container(
-            height: 48.0,
-          ),
-        ),
-      ],
-    );
-  }
-
-  /**
-   * 선택된 Day 표시
-   */
-  BoxDecoration setSelectedDayStyle(int index) {
-    int selectedDay = context
-        .watch<CalendarStore>()
-        .selectedDay;
-    if (selectedDay == index) {
-      return BoxDecoration(
-          color: Colors.black,
-          border: Border.all(
-            color: Colors.white,
-            width: 2.0,
-          ),
-          borderRadius: BorderRadius.all(Radius.circular(10.0)));
-    } else {
-      return BoxDecoration();
-    }
-  }
-
-  /**
-   * 캘린더 - 상단 AppBar
-   */
+  /// 캘린더 - header AppBar
   AppBar baseCalendarAppBar(BuildContext context) {
     return AppBar(
       leading: IconButton(
@@ -221,10 +76,8 @@ class _CalendarPageState extends State<CalendarPage> {
         ),
         child: Row(
           children: [
-            Text(context
-                .watch<CalendarStore>()
-                .selectedYearMonth),
-            Icon(Icons.arrow_drop_down)
+            Text(context.watch<CalendarStore>().selectedYearMonth),
+            Icon(Icons.arrow_drop_down),
           ],
         ),
       ),
@@ -259,4 +112,154 @@ class _CalendarPageState extends State<CalendarPage> {
       backgroundColor: Colors.black,
     );
   }
+
+  /// 캘린더 - body
+  CustomScrollView baseCalendarBody(
+      BuildContext context, AsyncSnapshot<List<Day>> snapshot) {
+    var size = MediaQuery.of(context).size;
+    print("size$size");
+    var days = snapshot.data;
+
+    return CustomScrollView(
+      scrollDirection: Axis.vertical,
+      slivers: <Widget>[
+        // 월 ~ 일 달력 헤더 그리드
+        SliverGrid(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 7, // 1개의 행에 보여줄 item개수
+            childAspectRatio:
+            2.5 / 1, // item의 가로세로비율 가로/세로 // 이거를 스크롤 될때 줄이거나 늘리거나 해야됨!
+          ),
+          delegate: SliverChildBuilderDelegate(
+                (BuildContext context, int index) {
+              return Container(
+                height: 100,
+                alignment: Alignment.center,
+                color: Colors.black,
+                child: Text(getWeekdays()[index].name,
+                    style: TextStyle(color: getWeekdays()[index].color)),
+              );
+            },
+            childCount: 7,
+          ),
+        ),
+        // 달력 메인 그리드
+        SliverGrid(
+
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 7, // 1개의 행에 보여줄 item개수
+            childAspectRatio: gridCellRatio ?? 1 / 2, // item의 가로세로비율 가로1: 세로2 // 이거를 스크롤 될때 줄이거나 늘리거나 해야됨! //
+          ),
+          delegate: SliverChildBuilderDelegate(
+
+                (BuildContext context, int index) {
+              // DayWidget
+              return Container(
+                alignment: Alignment.center,
+                color: Colors.black,
+                child: GestureDetector(
+                  onTap: () =>
+                      context.read<CalendarStore>().setSelectedDay(index),
+                  child: Opacity(
+                    opacity: days![index].isCurMonth ? 1.0 : 0.5,
+                    child: Container(
+                      decoration: setSelectedDayStyle(index),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: DayWidget(context, days!, index),
+                      ),
+                    ),
+                  ),
+                ),
+              );
+            },
+            childCount: 42, // TODO 42로 다시 변경해야함
+          ),
+        ),
+
+        // SliverFixedExtentList(delegate: SliverChildBuilderDelegate((BuildContext context, int index){
+        //   return ListTile(title: Text('List Item'));
+        // }), itemExtent: 10.0),
+
+        // bottomSheet 높이만큼 height를 지정해주자
+        SliverToBoxAdapter(
+          child: SizedBox(
+            height: 53.0,
+          ),
+        ),
+      ],
+    );
+  }
+
+  /// 캘린더 - footer 저장 입력바
+  Row baseCalendarBottomSheet(BuildContext context) {
+    return Row(
+      children: [
+        Flexible(
+          child: TextField(
+            // TODO
+            // 커서가 올라가면 borderRadius가 적용이 안된다.
+            // 커서올라같을때 시타일 지정하는게 있는지 찾아보기
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.blue.shade100,
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(30),
+                borderSide: BorderSide(
+                  color: Colors.green,
+                  width: 1.0,
+                ),
+              ),
+            ),
+            controller: _docContentEditController,
+          ),
+        ),
+        ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              foregroundColor:
+                  Theme.of(context).colorScheme.onSecondaryContainer,
+              backgroundColor: Theme.of(context).colorScheme.secondaryContainer,
+            ).copyWith(elevation: ButtonStyleButton.allOrNull(0.0)),
+            onPressed: () {},
+            // 키보드 활성 여부에 따라 버튼 아이콘 변경
+            child: gf_isShowKeyboard(context)
+                ? IconButton(
+                    icon: Icon(Icons.check),
+                    onPressed: () {
+                      print('click check butotn ');
+                      // saveDoc DocType >>  일정, 메모, todo, note post
+                      context
+                          .read<CalendarStore>()
+                          .saveDoc(_docContentEditController.text);
+                    },
+                  )
+                : IconButton(
+                    icon: Icon(Icons.add),
+                    onPressed: () {
+                      // TODO 상세 입력폼 나오게 분리하기
+                      print('click add butotn ');
+                    },
+                  ),
+        ),
+      ],
+    );
+  }
+
+  /// 선택된 Day 표시 - 스타일변경
+  BoxDecoration setSelectedDayStyle(int index) {
+    int selectedDay = context.watch<CalendarStore>().selectedDay;
+    if (selectedDay == index) {
+      return BoxDecoration(
+          color: Colors.black,
+          border: Border.all(
+            color: Colors.white,
+            width: 2.0,
+          ),
+          borderRadius: BorderRadius.all(Radius.circular(10.0)));
+    } else {
+      return BoxDecoration();
+    }
+  }
+
+
 }
--- lib/stores/CalendarStore.dart
+++ lib/stores/CalendarStore.dart
@@ -1,75 +1,80 @@
-import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
-import 'package:me/services/calendar/model/DocType.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert';
 
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:me/services/calendar/model/Document.dart';
-import 'package:me/services/calendar/model/Weekday.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../services/calendar/model/Day.dart';
 
 class CalendarStore extends ChangeNotifier {
+  String selectedYearMonth =
+      [DateTime.now().year, DateTime.now().month].join('.');
+  int selectedDay = DateTime.now().weekday == 7 ? 0 : DateTime.now().weekday;
+  late Future<List<Day>> selectedMonthCalendar = Future(() => [Day(DateTime.now())]);
 
-  String selectedYearMonth;
-  int selectedDay;
-  Future<List<Day>> selectedMonthCalendar;
-
-  //현재 연월로 초기화 // TODO 생성자 새로 정의 필요해 보임..
-  CalendarStore()
-      : selectedYearMonth = '',
-        selectedDay = 0,
-        selectedMonthCalendar = Future(() => []) {
-    this.selectedDay = DateTime.now().weekday == 7 ? 0 : DateTime.now().weekday;
-    this.selectedYearMonth =
-        [DateTime.now().year, DateTime.now().month].join('.');
-    this.selectedMonthCalendar =
-        makeCalendar(DateTime.now().year, DateTime.now().month);
-  }
-
+  /// 캘린더
+  /// 연, 월 선택
   setYearMonth(String value) {
     selectedYearMonth = value;
     notifyListeners();
   }
 
+  /// 캘린더
+  /// 오늘날짜 선택
   setToday() {
     DateTime now = DateTime.now();
     setDay(now.year, now.month, now.day);
   }
 
+  /// 캘린더
+  /// 날짜 선택
   setDay(int year, int month, int day) {
     // AppBar 상단에 선택된 년월
     selectedYearMonth = [year, month].join('.');
 
     // 달력 만들기
-    makeCalendar(year, month);
+    makeCalendar(year: year, month: month);
 
     // 상태값 감시
     notifyListeners();
   }
 
+  /// 캘린더
+  /// 선택된 날짜의 인텍스 저장
   setSelectedDay(int index) {
-    this.selectedDay = index;
+    selectedDay = index;
 
     // 상태값 감시
     notifyListeners();
   }
 
-  
-  Future<List<Day>> makeCalendar(int year, int month) async {
+  /// 캘린더
+  /// 캘린더 조회
+  /// 1.날짜 생성
+  /// 2. Documnet 로컬 DB 조회
+  Future<List<Day>> makeCalendar({int? year, int? month}) async {
+    print("makeCalendar");
+
+    // year, month 초기화
+    year ??= DateTime.now().year;
+    month ??= DateTime.now().month;
+
     var storage = await SharedPreferences.getInstance();
 
-    List<Day> calendar = [];
+    List<Day> newCalendar = [];
     DateTime firstDay = DateTime(year, month, 1);
     int startWeekDay = firstDay.weekday;
 
     // TODO Day객체에 List<Document> 넣어주기
-   var docsOfMonth = [];
+    var docsOfMonth = [];
 
-   // TODO 얘를 한번 가져오고 + 전월, 다음월것도 가져온다음에 합치기
+    // TODO 얘를 한번 가져오고 + 전월, 다음월것도 가져온다음에 합치기
     // 이전, 다음 월 추출하기
-    var prevYearMonth = DateFormat('yyyyMM').format(DateTime(year, month-1, 1));
-    var nextYearMonth = DateFormat('yyyyMM').format(DateTime(year, month+1, 1));
+    var prevYearMonth =
+        DateFormat('yyyyMM').format(DateTime(year, month - 1, 1));
+    var nextYearMonth =
+        DateFormat('yyyyMM').format(DateTime(year, month + 1, 1));
 
     var daysOfMonth = storage.getStringList(selectedYearMonth);
     var daysOfPrevMonth = storage.getStringList(prevYearMonth);
@@ -85,43 +90,44 @@ class CalendarStore extends ChangeNotifier {
     // }
     // print('docsOfMonth: $docsOfMonth');
 
-
     for (var day = 0; day < 42; day++) {
       // 첫번째주이고, 시작하는 요일이 startWeekDay인가?
       if (day < 7 && startWeekDay == day) {
-        calendar.add(Day(firstDay));
+        newCalendar.add(Day(firstDay));
       } else {
         DateTime anotherDay =
             DateTime(year, month, 1).add(Duration(days: day - startWeekDay));
-        calendar.add(Day(anotherDay));
+        newCalendar.add(Day(anotherDay));
       }
     }
-
-    return calendar;
+    print("calendar: $newCalendar");
+    // selectedMonthCalendar = newCalendar as Future<List<Day>>;
+    return newCalendar;
   }
 
   // TODO Parameter 수정될 듯!
   // 1. 일반 text 저장
   // TODO 2. type도 지정해야 함 >>> 상세 팝업 개발시
   // TODO 3. 추가, 수정, 삭제 여부 포함 되어야 할듯 >>> 상세 팝업 개발시
+  /// 캘린더
+  /// Document 저장
+  /// type: TODO,
   void saveDoc(String doc) async {
     var storage = await SharedPreferences.getInstance();
 
     // 생성일 Formating
     var nowDate = DateFormat('yyyy_MM_dd_HH_mm_ss').format(DateTime.now());
 
-    // var year = selectedMonthCalendar.then((value) => value[selectedDay].year.toString()).then((value) => value);
-    // var month = selectedMonthCalendar.then((value) => value[selectedDay].month.toString()).then((value) => value);
-    // var day = selectedMonthCalendar.then((value) => value[selectedDay].day.toString()).then((value) => value);
-
-
     late var year = '';
     late var month = '';
     late var day = '';
 
-    selectedMonthCalendar.then((value) => year = value[selectedDay].year.toString());
-    selectedMonthCalendar.then((value) => month = value[selectedDay].month.toString());
-    selectedMonthCalendar.then((value) => day = value[selectedDay].day.toString());
+    selectedMonthCalendar
+        .then((value) => year = value[selectedDay].year.toString());
+    selectedMonthCalendar
+        .then((value) => month = value[selectedDay].month.toString());
+    selectedMonthCalendar
+        .then((value) => day = value[selectedDay].day.toString());
 
     var yearMonth = year + month;
     var yearMonthDay = year + month + day;
@@ -144,25 +150,25 @@ class CalendarStore extends ChangeNotifier {
     // Document 저장
     storage.setString(document.docId, jsonEncode(json));
 
-
     // FIXME 저장 확인
     var keys = storage.getKeys();
     keys.forEach((element) {
-      print(element);
+      print("key: $element");
       var value = storage.get(element);
       print(value.toString());
       storage.remove(element);
     });
-
-
   }
 
-  // document를 저장하기전에 day, month를 parentKey로 저장
-  // 월 : [날짜, ...]
-  // ex >>> 202212 : [20221201, 20221208]
-  // 날짜 : [docId, ...]
-  // ex >>> 20221201 : [20221201-TODO-2022_12_01_14_02_22, ]
-  void saveParentKey(SharedPreferences storage, String yearMonthDay, String docId) {
+  /// 캘린더
+  /// Document Key저장
+  /// document를 저장하기전에 day, month를 parentKey로 저장
+  /// 월 : [날짜, ...]
+  /// ex >>> 202212 : [20221201, 20221208]
+  /// 날짜 : [docId, ...]
+  /// ex >>> 20221201 : [20221201-TODO-2022_12_01_14_02_22, ]
+  void saveParentKey(
+      SharedPreferences storage, String yearMonthDay, String docId) {
     bool hasDay = storage.containsKey(yearMonthDay);
     List<String> docs = [];
     docs = (hasDay ? storage.getStringList(yearMonthDay) : [])!;
