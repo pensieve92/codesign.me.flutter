caff30af1827d1fa1277a8efa367301a4d6c19dd
byeonghyeon.jeon <pensieve92@gmail.com>
2023-06-25 12:17:40 +0900

    Fix Calendar - 햄버거 메뉴 Drawer로 변경

--- lib/pages/calendar/CalendarPageV2.dart
+++ lib/pages/calendar/CalendarPageV2.dart
@@ -44,24 +44,24 @@ class _CalendarPageV2State extends State<CalendarPageV2> {
           child: Text("drawer"),
         ),
       ),
-      // body: SingleChildScrollView(
-      //   physics: const NeverScrollableScrollPhysics(),
-      //   child: ConstrainedBox(
-      //     constraints: BoxConstraints(
-      //       minWidth: MediaQuery.of(context).size.width,
-      //       minHeight: MediaQuery.of(context).size.height,
-      //     ),
-      //     child: IntrinsicHeight(
-      //       child: Column(
-      //         mainAxisSize: MainAxisSize.max,
-      //         children: <Widget>[
-      //           Expanded(child: CalendarBodyV4())
-      //           // CONTENT HERE
-      //         ],
-      //       ),
-      //     ),
-      //   ),
-      // ),
+      body: SingleChildScrollView(
+        physics: const NeverScrollableScrollPhysics(),
+        child: ConstrainedBox(
+          constraints: BoxConstraints(
+            minWidth: MediaQuery.of(context).size.width,
+            minHeight: MediaQuery.of(context).size.height,
+          ),
+          child: IntrinsicHeight(
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                Expanded(child: CalendarBodyV4())
+                // CONTENT HERE
+              ],
+            ),
+          ),
+        ),
+      ),
       // TODO 키보드가 없으면 추가 버튼이고
       // TODO 키보드가 있으면 생성 버튼으로
       bottomSheet: Container(
